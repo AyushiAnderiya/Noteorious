0ea6075b54654b55cf166b979873e9230723b9a1
AyushiAnderiya <ayushianderiya14@gmail.com>
2025-01-31 23:27:12 +0530

    Designing the frontend

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // fir mena views k andar file bnai or usko call kara
 const express=require ('express');
 const app=express();
+const path=require ('path');
 app.set('view engine','ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
