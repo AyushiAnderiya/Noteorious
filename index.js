d40bf6db08ed821f14495b1145471f3f69dc49d2
AyushiAnderiya <ayushianderiya14@gmail.com>
2025-02-01 00:55:43 +0530

    Creating the task field

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const express=require ('express');
 const app=express();
 const path=require ('path');
+const fs=require('fs');
+// fs file ko read karna k liya use hota h 
 app.set('view engine','ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -15,6 +17,20 @@ app.use((req,res,next)=>{
 //     res.send("started");
 // })
 app.get('/',(req,res)=>{
-    res.render("index");
+    fs.readdir(`./files`,function (err,files) {
+    res.render("index",{files:files});        
+    })
+    
+})
+//spilit se sb alag alag array me bn jata h or fir uunhi array ko apan join kar leta h
+app.post('/create',(req,res)=>{
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,function(err){
+        if(err)
+        console.error(err)
+        else
+        res.redirect("/");
+    })
+    
+    
 })
 app.listen(3000);
